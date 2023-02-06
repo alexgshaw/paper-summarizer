056bab963951881e60f8c801780cf2e7c325601c
Alex Shaw <alexgshaw64@gmail.com>
2023-02-05 19:43:24 -0700

    Add flag for double endlines.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ openai.api_key = os.environ.get("OPENAI_API_KEY")
 MAX_PARAGRAPH_LENGTH = 200
 
 
-def summarize(text: str):
+def summarize(text: str) -> str:
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt=f'Summarize the following paragraph into simple English:\n\n"{text}"\n\nSummary:',
@@ -24,14 +24,16 @@ def summarize(text: str):
     return response["choices"][0]["text"]
 
 
-def main(title: str):
+def main(title: str, has_double_endlines: bool):
     directory = os.path.join("papers", title)
 
+    sep = "\n\n" if has_double_endlines else "\n"
+
     with open(os.path.join(directory, "paper.txt"), "r") as file:
-        paragraphs = file.readlines()
+        paragraphs = file.read().split(sep)
 
     paragraphs = [
-        paragraph.strip()
+        paragraph.strip().replace("\n", " ")
         for paragraph in paragraphs
         if paragraph.strip() != "" and len(paragraph) > MAX_PARAGRAPH_LENGTH
     ]
@@ -63,6 +65,14 @@ if __name__ == "__main__":
         help="Path to the paper to summarize",
     )
 
+    # add a flag that indicates double endlines when present
+    parser.add_argument(
+        "-d",
+        "--double",
+        action="store_true",
+        help="Paragraphs are seperated by double endlines",
+    )
+
     args = parser.parse_args()
 
-    main(title=args.title)
+    main(title=args.title, has_double_endlines=args.double)
